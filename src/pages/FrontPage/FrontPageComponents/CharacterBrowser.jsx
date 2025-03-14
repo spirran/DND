4efbf1ebd20f908fc46c85ad3a2db1c3d86560ae
gustavhedegard <gustav.selin@gmail.com>
2025-03-14 09:52:40 +0100

    add minor changes

--- src/pages/FrontPage/FrontPageComponents/CharacterBrowser.jsx
+++ src/pages/FrontPage/FrontPageComponents/CharacterBrowser.jsx
@@ -4,7 +4,7 @@ function CharacterBrowser() {
     return (
         <>
             <div className='browser-wrapper'>
-                <h2>Character Browser</h2>
+                <h2>Characters</h2>
                 <BrowserCard text="HERE"/>
                 <BrowserCard text="HERE"/>
                 <BrowserCard text="HERE"/>
